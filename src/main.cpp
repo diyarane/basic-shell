23647fa8933e03836a50712c811cf3ab452f4e01
diyarane <diyarane0912@gmail.com>
2025-10-28 21:11:43 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main() {
 
     std::string input;
     std::getline(std::cin, input);
+    if(input == "exit 0") return 0;
     std::cout << input << ": command not found" << std::endl;
   }
 }
